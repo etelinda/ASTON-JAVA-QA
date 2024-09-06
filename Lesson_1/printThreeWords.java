4440b1f355a56e7a1410768b53058a7c8dea6e35
Garipova Asiya <41074332+etelinda@users.noreply.github.com>
2024-09-06 15:39:22 +0300

    Update printThreeWords.java

--- Lesson_1/printThreeWords.java
+++ Lesson_1/printThreeWords.java
@@ -1,6 +1,8 @@
+package Lesson_1
 public class main {
     public static void printThreeWords()    }
     static void printThreeWords() {
         System.out.println("orange");
         System.out.println("banana");
         System.out.println("apple");
+    }    
